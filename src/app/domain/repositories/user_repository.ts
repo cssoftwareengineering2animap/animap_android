76d856cb870485054cc77ace83e8cff37e1b44bf
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-10-11 23:37:55 -0300

    feat: add login use case

--- src/app/domain/repositories/user_repository.ts
+++ src/app/domain/repositories/user_repository.ts
@@ -2,9 +2,13 @@ import { Observable } from "rxjs"
 import { Envelope } from "../../core/types/envelope"
 import { User } from "../entities/user_entity"
 import { RegisterUserDto } from "../use_cases/user/create_user/register_user_dto"
+import { LoginDto } from "../use_cases/user/login/login.dto"
+
+export type Token = "string"
 
 export interface UserRepository {
   create: (data: RegisterUserDto) => Observable<Envelope<User>>
+  login: (data: LoginDto) => Observable<Envelope<Token>>
 }
 
 export const UserRepositoryToken = "UserRepository"
