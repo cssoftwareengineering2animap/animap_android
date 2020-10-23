b8dfeed7dcaa6a44bbb05b683eec2812a17969e5
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-10-22 21:18:43 -0300

    fix: forgot Injectable decorator

--- src/app/infra/http/error_interceptor.ts
+++ src/app/infra/http/error_interceptor.ts
@@ -5,6 +5,7 @@ import {
   HttpRequest,
   HttpErrorResponse,
 } from "@angular/common/http"
+import { Injectable } from "@angular/core"
 import { Router } from "@angular/router"
 import { StatusCodes } from "http-status-codes"
 
@@ -12,6 +13,7 @@ import { Observable, throwError } from "rxjs"
 
 import { retry, catchError } from "rxjs/operators"
 
+@Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
   constructor(private readonly route: Router) {}
 
