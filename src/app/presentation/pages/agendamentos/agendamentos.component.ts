ac1bea1c8a033cbfb7e62679ce7762f55f41c439
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2020-10-25 18:57:25 -0300

    wip

--- src/app/presentation/pages/agendamentos/agendamentos.component.ts
+++ src/app/presentation/pages/agendamentos/agendamentos.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core"
 import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { ConfirmationService } from "primeng/api"
-import { Pet } from "./models/pet.model";
-
+import { Pet } from "./models/pet.model"
 
 @Component({
   selector: "app-agendamentos",
@@ -10,37 +9,38 @@ import { Pet } from "./models/pet.model";
   styleUrls: ["./agendamentos.component.scss"],
 })
 export class AgendamentosComponent implements OnInit {
-
-  public pets: Pet[];
+  public pets: Pet[]
 
   faBars = faBars
+
   public dropdownToggle = false
 
   constructor(private confirmationService: ConfirmationService) {
     this.pets = [
-      new Pet(1,1,3,'Nana', 'Pintcher'),
-      new Pet(2,2,4,'Bidu', 'Beagle'),
-      new Pet(3,3,1,'Branca', 'Chow-chow')
+      new Pet(1, 1, 3, "Nana", "Pintcher"),
+      new Pet(2, 2, 4, "Bidu", "Beagle"),
+      new Pet(3, 3, 1, "Branca", "Chow-chow"),
     ]
   }
 
   confirm(nomeAnimal: string) {
     this.confirmationService.confirm({
       message: `Deseja solicitar um agendamento com ${nomeAnimal}?`,
-      acceptLabel: 'Sim',
-      rejectLabel: 'Não',
+      acceptLabel: "Sim",
+      rejectLabel: "Não",
       accept: () => {
-        //Actual logic to perform a confirmation
+        // Actual logic to perform a confirmation
       },
     })
   }
 
   ngOnInit(): void {}
+
   public toggle() {
     this.dropdownToggle = !this.dropdownToggle
   }
 
   public getIdade(idade: number): string {
-    return `${idade} ${(idade <= 1) ? 'ano' : 'anos'}`
+    return `${idade} ${idade <= 1 ? "ano" : "anos"}`
   }
 }
