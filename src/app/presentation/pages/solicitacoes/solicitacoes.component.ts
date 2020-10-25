46213a326128ef670fd6ad9b0d3d6b603f865f02
Cherymoon <nemseialistar@gmail.com>
2020-10-25 18:14:22 -0300

    add Tela Solicitacoes

---
+++ src/app/presentation/pages/solicitacoes/solicitacoes.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { ConfirmationService } from "primeng/api"
+import { Solicitacao } from "./models/solicitacao.model"
+
+@Component({
+  selector: "app-solicitacoes",
+  templateUrl: "./solicitacoes.component.html",
+  styleUrls: ["./solicitacoes.component.scss"],
+})
+export class SolicitacoesComponent implements OnInit {
+  public solicitacoes: Solicitacao[]
+
+  faBars = faBars
+  public dropdownToggle = false
+
+  constructor(private confirmationService: ConfirmationService) {
+    this.solicitacoes = [
+      new Solicitacao(1, "Bidu", "Ana"),
+      new Solicitacao(1, "Romeu", "Bruno"),
+      new Solicitacao(3, "Branca", "Carol"),
+    ]
+  }
+
+  confirm(nomeAnfitriao: string) {
+    this.confirmationService.confirm({
+      message: `Deseja aceitar a solicitação de ${nomeAnfitriao}?`,
+      acceptLabel: "Sim",
+      rejectLabel: "Não",
+      accept: () => {
+        //Actual logic to perform a confirmation
+      },
+    })
+  }
+
+  ngOnInit(): void {}
+  public toggle() {
+    this.dropdownToggle = !this.dropdownToggle
+  }
+}
